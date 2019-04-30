9e60d2f25098e59ce5e0ccd560fbcf0e81e5ce6d
Phil DHK <p.potraz@gmx.de>
2019-04-30 12:08:55 +0200

    GrAmMaTiK

--- php/buildFooter.php
+++ php/buildFooter.php
@@ -2,4 +2,4 @@
 //baut den Footer auf.
 echo '
     <p class="float-right"><a href="#">Back to top</a></p>
-    <p>&copy; 2019 Schwanger Cocktails. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>';
+    <p>&copy; 2019 Schwangere Cocktails. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>';
