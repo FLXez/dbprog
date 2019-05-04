c5d6c2e9449f4aebd7b66d916806e309083015d8
Schnietzel <mryoced@gmail.com>
2019-05-04 08:12:14 +0200

    eta_img.php zu img.php umbenannt, damit auch Cocktail-Bilder ohne Verwirrung aufgerufen werden können

--- php/img.php
+++ php/img.php
@@ -4,12 +4,17 @@ if (isset($_GET['eta_img_id'])) {
     $result = $pdo->prepare("SELECT img FROM etablissement_img where id = :img_id");
     if ($result->execute(array('img_id' => $_GET['eta_img_id']))) {
         $imgFetch = $result->fetch();
-        echo $imgFetch[0]; //this prints the image data, transforming the image.php to an image
+        echo $imgFetch[0];
         
     } 
 }
-	else {
-	echo '../res/mexcal.jpg';
+elseif (isset($_GET['cock_img_id']))
+{
+	$result = $pdo->prepare("SELECT img FROM cocktail_img WHERE id = :img_id");
+	if ($result->execute(array('img_id' => $_GET['cock_img_id'])))
+	{
+		$imgFetch = $result->fetch();
+		echo $imgFetch[0];
 	}
-// you can put an "else" here to do something on error...
+}
 ?>
