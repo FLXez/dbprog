14d4451f146a7bfc0b0e6f1f89c0fd1e9ce09627
Phil DHK <p.potraz@gmx.de>
2019-05-07 14:26:37 +0200

    hilfe ceddi, das will nicht

--- site/cocktail_new.php
+++ site/cocktail_new.php
@@ -101,10 +101,14 @@ if ($angemeldet) {
 							<label for="nameCock">Name</label>
 							<input type="text" maxlength="50" class="form-control" id="nameCock" name="nameCock"  placeholder="Cocktail">
 						</div>
-						<div class="form-group">
-							<label for="image">Bild</label>
-							<input type="file" name="file" id="image" class="form-control-file"> 	
-						</div>						
+
+						<div class="input-group mb-3">
+							<div class="custom-file">
+								<input type="file" class="custom-file-input" id="file">
+								<label class="custom-file-label" for="file" aria-describedby="file" > Choose file</label>
+							</div>
+						</div>
+
 						<div class="form-group">
 							<label for="beschreibungCock">Beschreibung</label>
 							<input type="text" maxlength="50" class="form-control" id="beschreibungCock" name="beschreibungCock"  placeholder="Beschreibung">
