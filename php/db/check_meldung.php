72fd32aa30bc2a071610c25d343ffdd06b423ab2
FLXez <37533413+Swoodox@users.noreply.github.com>
2019-05-29 16:01:10 +0200

    nur einmal meldbar

---
+++ php/db/check_meldung.php
@@ -0,0 +1,87 @@
+<?php
+function check_meld_user($userId)
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=dbprog', 'root', '');
+    $statement = $pdo->prepare(
+        "SELECT '1'
+         FROM meldung
+         WHERE cock_id IS NULL
+           AND etab_id IS NULL
+           AND meldung_art = 'user'
+           AND status = 0
+           AND user_id= :user_id"
+    );
+    $result = $statement->execute(array('user_id' => $userId));
+    $GLOBALS['meldung_vorhanden'] = $statement->fetch();
+    $pdo = NULL;
+}
+
+function check_meld_etab($etabId)
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=dbprog', 'root', '');
+    $statement = $pdo->prepare(
+        "SELECT '1'
+         FROM meldung
+         WHERE cock_id IS NULL
+           AND etab_id = :etab_id 
+           AND meldung_art = 'etab'
+           AND status = '0'
+           AND user_id= IS NULL"
+    );
+    $result = $statement->execute(array('etab_id' => $etabId));
+    $GLOBALS['meldung_vorhanden'] = $statement->fetch();
+    $pdo = NULL;
+}
+
+function check_meld_etabBew($etabId, $userId)
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=dbprog', 'root', '');
+    $statement = $pdo->prepare(
+        "SELECT '1'
+         FROM meldung
+         WHERE cock_id IS NULL
+           AND etab_id = :etab_id 
+           AND meldung_art = 'etab_bew'
+           AND status = 0
+           AND user_id= :user_id"
+    );
+    $result = $statement->execute(array('etab_id' => $etabId, 'user_id' => $userId));
+    $GLOBALS['meldung_vorhanden'] = $statement->fetch();
+    $pdo = NULL;
+}
+
+
+
+function check_meld_cock($cockId)
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=dbprog', 'root', '');
+    $statement = $pdo->prepare(
+        "SELECT '1'
+         FROM meldung
+         WHERE cock_id = :cock_id
+           AND etab_id IS NULL
+           AND meldung_art = 'cock'
+           AND status = 0
+           AND user_id IS NULL"
+    );
+    $result = $statement->execute(array('cock_id' => $cockId));
+    $GLOBALS['meldung_vorhanden'] = $statement->fetch();
+    $pdo = NULL;
+}
+
+function check_meld_cockBew($cockId, $userId)
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=dbprog', 'root', '');
+    $statement = $pdo->prepare(
+        "SELECT '1'
+         FROM meldung
+         WHERE cock_id = :cock_id
+           AND etab_id IS NULL
+           AND meldung_art = 'cock_bew'
+           AND status = 0
+           AND user_id= :user_id"
+    );
+    $result = $statement->execute(array('cock_id' => $cockId, 'user_id' => $userId));
+    $GLOBALS['meldung_vorhanden'] = $statement->fetch();
+    $pdo = NULL;
+}
--- php/melden.php
+++ php/melden.php
@@ -1,23 +1,43 @@
 <?php
 session_start();
+include('db/check_meldung.php');
+
 if (isset($_SESSION['userId'])) {
     $melderId = $_SESSION['userId'];
-    if (isset($_GET['meldungArt'])) {        
+    if (isset($_GET['meldungArt'])) {
         $meldungArt = $_GET['meldungArt'];
         if ($meldungArt == "user") {
             $userId = $_GET['userId'];
+            $etabId = NULL;
+            $cockId = NULL;
+            check_meld_user($userId);
         } elseif ($meldungArt == "etab") {
+            $userId = NULL;            
             $etabId = $_GET['etabId'];
+            $cockId = NULL;
+            check_meld_etab($etabId);
         } elseif ($meldungArt == "etab_bew") {
             $userId = $_GET['userId'];
             $etabId = $_GET['etabId'];
+            $cockId = NULL;
+            check_meld_etabBew($etabId, $userId);
         } elseif ($meldungArt == "cock") {
+            $userId = NULL;
+            $etabId = NULL;
             $cockId = $_GET['cockId'];
+            check_meld_cock($cockId);
         } elseif ($meldungArt == "cock_bew") {
             $userId = $_GET['userId'];
+            $etabId = NULL;            
             $cockId = $_GET['cockId'];
+            check_meld_cockBew($cockId, $userId);
         }
-        include('db/insert_meldung.php');
+        if ($meldung_vorhanden) {
+            $_SESSION['message'] = "Eintrag wurde bereits gemeldet!";
+            $_SESSION['error'] = true;
+        } else {
+            include('db/insert_meldung.php');
+        }
     }
 }
 header("Location: " . $_SESSION['source']);
